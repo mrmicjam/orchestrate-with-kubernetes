dfae09996865852b9d1542acaaa3fc81880bf0d4
micah <dataconcise@gmail.com>
2019-09-18 15:54:04 -0300

    127 - 0

--- flask_test/app.py
+++ flask_test/app.py
@@ -13,5 +13,5 @@ def root():
     return render_template('index.html', products=products)
 
 if __name__ == "__main__":
-  app.run(host='127.0.0.1', port=8080)
+  app.run(host='0.0.0.0', port=8080)
 
