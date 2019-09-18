2d7e3a166bc2b2ec69e4fe757a424cd14322bc92
micah <dataconcise@gmail.com>
2019-09-18 12:59:25 -0300

    add a simple flask app to demonstrate datastore access

---
+++ flask_test/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from google.cloud import datastore
+
+
+datastore_client = datastore.Client()
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    products = datastore_client.query(kind="deal_product").fetch(limit=5)
+    return render_template('index.html', products=products)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=80)
+
